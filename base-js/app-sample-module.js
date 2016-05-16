5cb3952c8969a809be4a7ba02fc177aa717dbfa8
Brian Ritter <brian@ambrdetroit.com>
2016-05-15 22:51:12 -0400

    added base js

---
+++ base-js/app-sample-module.js
@@ -0,0 +1,12 @@
+(function() {
+	
+	"use strict";
+	
+	window.app = window.app || {};
+	window.app.module = {
+		// module stuff
+	};
+	
+})();
+
+
---
+++ base-js/app.js
@@ -0,0 +1,17 @@
+(function() {
+	
+	"use strict";
+	
+	// the app javascript
+	window.app = window.app || {};
+	app = {
+		data : {},
+		func : {}
+	};
+	
+	// on document ready
+	document.addEventListener("DOMContentLoaded", function() {
+		// add code here
+	});
+	
+})();
