abe46cc588b2e522d2f6bdb227e6a364f95dcdc8
Brian Ritter <brian@ambrdetroit.com>
2016-05-15 23:06:53 -0400

    starting work on ambrjs

---
+++ ambrjs-framework/ambrjs-routing.js
@@ -0,0 +1,34 @@
+(function() {
+	
+	"use strict";
+	
+	// routing
+	window.ambrjs = window.ambrjs || {};
+	if(!window.ambrjs.routing) {
+		window.ambrjs.routing = {
+			
+				
+				
+		};
+	}
+	
+	/*
+	window.ambrjs.routing = {
+		
+	}
+	*/
+	// the app javascript
+	/*
+	window.app = window.app || {};
+	app = {
+		data : {},
+		func : {}
+	};
+	*/
+	
+	// on document ready
+	document.addEventListener("DOMContentLoaded", function() {
+		// add code here
+	});
+	
+})();
---
+++ ambrjs-framework/ambrjs.js
@@ -0,0 +1,19 @@
+(function() {
+	
+	"use strict";
+	
+	// the app javascript
+	/*
+	window.app = window.app || {};
+	app = {
+		data : {},
+		func : {}
+	};
+	*/
+	
+	// on document ready
+	/*document.addEventListener("DOMContentLoaded", function() {
+		// add code here
+	});*/
+	
+})();
